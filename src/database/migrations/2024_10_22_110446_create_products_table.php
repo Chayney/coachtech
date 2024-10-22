e224c7e2c187f9ada7da7380117653cdf7b702ca
Chayney <chunchunmaru33333@gmail.com>
2024-10-22 21:45:10 +0900

    商品登録用テーブルの作成

--- src/database/migrations/2024_10_22_110446_create_products_table.php
+++ src/database/migrations/2024_10_22_110446_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('profile_id')->constrained()->cascadeOnDelete();
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->foreignId('conditon_id')->constrained()->cascadeOnDelete();
             $table->string('name');
