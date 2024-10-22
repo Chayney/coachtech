9dc8d3ffcbdcf9a05c3e87a87eb7643e44ac6eae
Chayney <chunchunmaru33333@gmail.com>
2024-10-22 21:39:11 +0900

    お気に入り機能用中間テーブルの作成

--- src/database/migrations/2024_10_22_123032_create_favorites_table.php
+++ src/database/migrations/2024_10_22_123032_create_favorites_table.php
@@ -15,7 +15,7 @@ class CreateFavoritesTable extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('profile_id')->constrained()->cascadeOnDelete();
             $table->foreignId('product_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
