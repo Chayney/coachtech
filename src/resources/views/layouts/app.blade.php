ad2dbc2f07457bb7277d6178c880b656ea80f42e
Chayney <chunchunmaru33333@gmail.com>
2024-10-24 18:54:02 +0900

    ヘッダーのレイアウト調整

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>coachtech</title>
-  <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   @yield('css')
 </head>
 
@@ -15,18 +15,39 @@
     <div class="header__inner">
       <div class="header-utilities">
         <a class="header__logo" href="/">
-          <img src="{{ asset('images/logo.svg') }}"
+          <img src="{{ asset('images/logo.svg') }}">
         </a>
+        <input class="search__box" type="text" name="keyword" placeholder="なにをお探しですか?" value="">
         <nav>
           <ul class="header-nav">
+          @if (Request::is('register'))
             <li class="header-nav__item">
-              <a class="header-nav__link" href="/mypage">マイページ</a>
-            </li>
-            <li class="header-nav__item">
-              <form>
+              <form action="logout" method="post">
                 <button class="header-nav__button">ログアウト</button>
               </form>
             </li>
+            <li class="header-nav__item">
+              <a class="header-nav__link" href="/mypage">マイページ</a>
+            </li>
+          @else
+          </li>
+            <li class="header-nav__item">
+              <a class="header-nav__link" href="/login">ログイン</a>
+            </li>
+            </li>
+            <li class="header-nav__item">
+              <a class="header-nav__link" href="/register">会員登録</a>
+            </li>
+          @endif
+          @if (Auth::check())
+            <li class="header-nav__white">
+              <a class="header-item__link" href="/">出品</a>
+            </li>
+          @else
+            <li class="header-nav__white">
+              <a class="header-item__link" href="/login">出品</a>
+            </li>
+          @endif
           </ul>
         </nav>
       </div>
