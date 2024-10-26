eab49002c9421524986f8907727a08aca2255651
Chayney <chunchunmaru33333@gmail.com>
2024-10-26 14:54:39 +0900

    マイページの作成

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -5,12 +5,16 @@
 @endsection
 
 @section('content')
+    <div class="profile">
+        @foreach ($products as $product)
+        <img class="profile_image" src="{{ $product['image'] }}">
+    </div>
     <div class="tabs">
         <button class="tab-button" data-target="tab1">出品した商品</button>
         <button class="tab-button" data-target="tab2">購入した商品</button>
     </div>
     <div class="parent__container">
-        @foreach ($products as $product)
+        
             <div class="child__container">
                 <form action="/item/" method="get">
                 <img class="product_image" src="{{ $product['image'] }}">
