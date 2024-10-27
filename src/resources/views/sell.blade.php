c66f02e53576d3a45f128c04aeda7e2ff58851d0
Chayney <chunchunmaru33333@gmail.com>
2024-10-27 18:53:36 +0900

    レイアウト調整

--- src/resources/views/sell.blade.php
+++ src/resources/views/sell.blade.php
@@ -36,7 +36,7 @@
       </div>
       <div class="form__group-content">
         <div class="form__input--text">
-          <select name="category_id">
+          <select class="select__box" name="category_id">
             <option disabled selected>必須</option>
             @foreach ($categories as $category)
               <option value="{{ $category['element']['id'] }}" {{ request('category') == $category->id ? 'selected' : '' }}>{{ $category['element']['name'] }}</option>
@@ -55,7 +55,7 @@
         <span class="form__label--item">商品の状態</span>
       </div>
       <div class="form__group-content">
-        <select name="condition_id">
+        <select class="select__box" name="condition_id">
           <option disabled selected>必須</option>
           @foreach ($conditions as $condition)
             <option value="{{ $condition['id'] }}" {{ request('condition') == $condition->id ? 'selected' : '' }}>{{ $condition['condition'] }}</option>
@@ -110,7 +110,8 @@
       </div>
       <div class="form__group-content">
         <div class="form__input--text">
-          <input type="text" name="price" placeholder="¥">
+          <input type="text" name="price">
+          <label class="input__label">¥</label>
         </div>
         <div class="form__error">
           @error('')
