b63cf75be4ba4fd3055373b0a9bf9aaa04b53666
Chayney <chunchunmaru33333@gmail.com>
2024-10-27 13:03:36 +0900

    カテゴリー項目テーブルの作成

--- src/database/migrations/2024_10_20_130200_create_elements_table.php
+++ src/database/migrations/2024_10_20_130200_create_elements_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateListsTable extends Migration
+class CreateElementsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateListsTable extends Migration
      */
     public function up()
     {
-        Schema::create('lists', function (Blueprint $table) {
+        Schema::create('elements', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateListsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('lists');
+        Schema::dropIfExists('elements');
     }
 }
