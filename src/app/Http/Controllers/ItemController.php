623b352c9d1887daf1c5bc3626fc0f6d3724190f
Chayney <chunchunmaru33333@gmail.com>
2024-10-27 13:13:47 +0900

    商品登録用コントローラーの作成

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -4,25 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Product;
+use App\Models\Item;
 use App\Models\Profile;
 use App\Models\Condition;
 use App\Models\Category;
-use App\Models\Item;
+use App\Models\Element;
 
-class ProductController extends Controller
+class ItemController extends Controller
 {
     public function index()
     {
         $user = Auth::user();
-        $products = Product::all();
-        return view('index', compact('products'));
+        $items = Item::all();
+        return view('index', compact('items'));
     }
 
     public function detail()
     {
-        $products = Product::with(['category.item','condition'])->get();
-        return view('detail', compact('products'));
+        $items = Item::with(['category.element','condition'])->get();
+        return view('detail', compact('items'));
     }
 
     public function sell()
