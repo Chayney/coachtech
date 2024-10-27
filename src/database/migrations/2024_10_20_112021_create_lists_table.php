cd5c4e4f5ae48f6b1815898ae9cda72f5185f1d9
Chayney <chunchunmaru33333@gmail.com>
2024-10-27 11:23:43 +0900

    カテゴリー項目テーブルの作成

--- src/database/migrations/2024_10_20_112021_create_lists_table.php
+++ src/database/migrations/2024_10_20_112021_create_lists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateItemsTable extends Migration
+class CreateListsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateItemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('items', function (Blueprint $table) {
+        Schema::create('lists', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('items');
+        Schema::dropIfExists('lists');
     }
 }
--- src/database/seeders/ProductsTableSeeder.php
+++ src/database/seeders/ProductsTableSeeder.php
@@ -123,5 +123,16 @@ class ProductsTableSeeder extends Seeder
             'image' => 'https://coachtech-matter.s3-ap-northeast-1.amazonaws.com/image/yakiniku.jpg'
         ];
         DB::table('products')->insert($param);
+
+        $param = [
+            'profile_id' => '1',
+            'category_id' => '2',
+            'condition_id' => '3',
+            'name' => '商品11',
+            'description' => '即購入大丈夫です。',
+            'price' => '300',
+            'image' => 'https://coachtech-matter.s3-ap-northeast-1.amazonaws.com/image/sushi.jpg'
+        ];
+        DB::table('products')->insert($param);
     }
 }
