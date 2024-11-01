e607b0be74300120a15de532646a1d36d46900b6
Chayney <chunchunmaru33333@gmail.com>
2024-11-01 20:51:53 +0900

    タブ切り替え表示設定

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -6,23 +6,53 @@
 
 @section('content')
     <div class="tabs">
-        <button class="tab-button" data-target="tab1">おすすめ</button>
         @if (Auth::check())
+            <button class="tab-button active" data-target="tab1">おすすめ</button>
             <button class="tab-button" data-target="tab2">マイリスト</button>
+        @else
+            <button class="tab-button-stable" data-target="tab3">おすすめ</button>
         @endif
     </div>
-    <div class="parent__container">
-        @foreach ($items as $item)
-            <div class="child__container">
-                <form action="/item/:item_id" method="get">
-                    <input type="hidden" name="id" value="{{ $item['id'] }}">
-                        @if (Str::startsWith($item['image'], 'images/'))
-                            <button><img class="product_image" src="{{ asset($item['image']) }}"></button>
-                        @else
-                            <button><img class="product_image" src="{{ asset( '/storage/' . $item['image']) }}"></button>
-                        @endif
-                </form>
+    <div class="tab-content">
+        @if (Auth::check())
+            <div id="tab1" class="tab-pane">
+                <div class="parent__container">
+                    @foreach ($items as $item)
+                        <div class="child__container">
+                            <form action="/item/:item_id" method="get">
+                                <input type="hidden" name="id" value="{{ $item['id'] }}">
+                                    @if (Str::startsWith($item['image'], 'images/'))
+                                        <button><img class="product_image" src="{{ asset($item['image']) }}"></button>
+                                    @else
+                                        <button><img class="product_image" src="{{ asset( '/storage/' . $item['image']) }}"></button>
+                                    @endif
+                            </form>
+                        </div>
+                    @endforeach
+                </div>
             </div>
-        @endforeach
+            <div id="tab2" class="tab-pane">
+            <div class="parent__container-right">
+            </div>  
+            </div>
+        @else
+            <div id="tab3">
+                <div class="parent__container">
+                    @foreach ($items as $item)
+                        <div class="child__container">
+                            <form action="/item/:item_id" method="get">
+                                <input type="hidden" name="id" value="{{ $item['id'] }}">
+                                    @if (Str::startsWith($item['image'], 'images/'))
+                                        <button><img class="product_image" src="{{ asset($item['image']) }}"></button>
+                                    @else
+                                        <button><img class="product_image" src="{{ asset( '/storage/' . $item['image']) }}"></button>
+                                    @endif
+                            </form>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        @endif
     </div>
+    <script src="{{ asset('js/index.js') }}" type="text/javascript"></script>
 @endsection
