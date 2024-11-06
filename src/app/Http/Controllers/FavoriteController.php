2a9a822e4003210d4ac126274fa59fd4bfe6c7a8
Chayney <chunchunmaru33333@gmail.com>
2024-11-06 20:42:25 +0900

    favoriteMarkedメソッドの追加

--- src/app/Http/Controllers/FavoriteController.php
+++ src/app/Http/Controllers/FavoriteController.php
@@ -3,13 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Favorite;
+use App\Models\Profile;
+use App\Models\Item;
 
 class FavoriteController extends Controller
 {
     public function store(Request $request)
     {
         $user = Auth::user();
+        $profile = Profile::where('user_id', $user->id)->first(['id']);
         $item_id = $request->item_id;
+        $favoritemark = Favorite::where('profile_id', $profile->id)->where('item_id', $item_id)->first();
+        if (!$favoritemark) {
+            Favorite::create([
+                'profile_id' => $profile->id,
+                'item_id' => $item_id
+            ]);
+
+            return redirect()->back();
+        }  
+    }
+
+    public function destroy(Request $request)
+    {
+        $user = Auth::user();
+        $profile = Profile::where('user_id', $user->id)->first(['id']);
+        $item_id = $request->item_id;
+        Favorite::where('profile_id', $profile->id)->where('item_id', $item_id)->delete();
+
+        return redirect()->back();
     }
 }
