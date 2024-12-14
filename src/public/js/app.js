9ef0b487a476780cde8793bb90b8b1feb0359573
Chayney <chunchunmaru33333@gmail.com>
2024-12-14 21:48:37 +0900

    検索バーの表示設定

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,8 @@
 document.getElementById('searchButton').addEventListener('click', function() {
     var searchBar = document.getElementById('searchBar');
     if (searchBar.style.display === 'none' || searchBar.style.display === '') {
-        searchBar.style.display = 'block'; // 検索バーを表示
+        searchBar.style.display = 'block';
     } else {
-        searchBar.style.display = 'none'; // 検索バーを非表示
+        searchBar.style.display = 'none';
     }
 });
