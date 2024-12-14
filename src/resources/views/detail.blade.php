1f22f6c5a7f22522900460be578e3648f7bceaef
Chayney <chunchunmaru33333@gmail.com>
2024-12-14 22:52:31 +0900

    レスポンシブデザイン調整

--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -19,66 +19,66 @@
                     <span class="label--item">商品名</span><br>
                     <span class="form__label--item">{{ $item['name'] }}</span><br>
                     <span class="form__label--item">¥{{ $item['price'] }}(値段)</span>
-                    @if (Auth::check())
-                        <div class="icon-group">
-                            @if ($item->favoriteMarked())
-                                <form action="/favorite/destroy" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <input type="hidden" name="item_id" value="{{ $item['id'] }}">
-                                    <button class="favorited" type="submit" onclick="return showAlert('本当にお気に入りを削除しますか？')">
-                                        <img class="favorited_image" src="{{ asset('images/yellow_star.jpg') }}">
-                                        <span>{{ $item->favorites_count }}</span>
-                                    </button>                      
-                                </form>
-                            @else
-                                <form action="/favorite/store" method="post">
-                                    @csrf
-                                    <input type="hidden" name="item_id" value="{{ $item['id'] }}">
-                                    <button class="favorite" type="submit">
-                                        <img class="favorite_image" src="{{ asset('images/star.jpg') }}">
-                                        <span>{{ $item->favorites_count }}</span>
-                                    </button>                            
-                                </form>
-                            @endif
-                            <form action="/comment" method="get">
-                                <input type="hidden" name="id" value="{{ $item['id'] }}">
+                </div>
+                @if (Auth::check())
+                    <div class="icon-group">
+                        @if ($item->favoriteMarked())
+                            <form action="/favorite/destroy" method="post">
+                                @csrf
+                                @method('DELETE')
                                 <input type="hidden" name="item_id" value="{{ $item['id'] }}">
-                                <button class="comment" type="submit" name="name" value="{{ $item['name'] }}">
-                                    <img class="comment_image" src="{{ asset('images/comment.jpg') }}">
-                                    <span>{{ $item->comments_count }}</span>
-                                </button>
+                                <button class="favorited" type="submit" onclick="return showAlert('本当にお気に入りを削除しますか？')">
+                                    <img class="favorited_image" src="{{ asset('images/yellow_star.jpg') }}">
+                                    <span>{{ $item->favorites_count }}</span>
+                                </button>                      
                             </form>
-                        </div>
-                        <form action="/purchase/{item_id}" method="get">
-                            <div class="form__button">
-                                <input type="hidden" name="id" value="{{ $item['id'] }}">
-                                <button class="form__button-submit" type="submit">購入する</button>
-                            </div>
-                        </form>
-                    @else
-                        <div class="icon-group">
-                            <div class="favorite-group">
-                                <button class="favorite" onclick="location.href='/login'">
+                        @else
+                            <form action="/favorite/store" method="post">
+                                @csrf
+                                <input type="hidden" name="item_id" value="{{ $item['id'] }}">
+                                <button class="favorite" type="submit">
                                     <img class="favorite_image" src="{{ asset('images/star.jpg') }}">
                                     <span>{{ $item->favorites_count }}</span>
-                                </button>
-                            </div>
-                            <form action="/comment" method="get">
-                                <input type="hidden" name="id" value="{{ $item['id'] }}">
-                                <input type="hidden" name="item_id" value="{{ $item['id'] }}">
-                                <button class="comment" type="submit" name="name" value="{{ $item['name'] }}">
-                                    <img class="comment_image" src="{{ asset('images/comment.jpg') }}">
-                                    <span>{{ $item->comments_count }}</span>
-                                </button>
+                                </button>                            
                             </form>
-                        </div>
-                        <div class="form__button" onclick="location.href='/login'">
+                        @endif
+                        <form action="/comment" method="get">
+                            <input type="hidden" name="id" value="{{ $item['id'] }}">
+                            <input type="hidden" name="item_id" value="{{ $item['id'] }}">
+                            <button class="comment" type="submit" name="name" value="{{ $item['name'] }}">
+                                <img class="comment_image" src="{{ asset('images/comment.jpg') }}">
+                                <span>{{ $item->comments_count }}</span>
+                            </button>
+                        </form>
+                    </div>
+                    <form action="/purchase/{item_id}" method="get">
+                        <div class="form__button">
                             <input type="hidden" name="id" value="{{ $item['id'] }}">
                             <button class="form__button-submit" type="submit">購入する</button>
                         </div>
-                    @endif
-                </div>
+                    </form>
+                @else
+                    <div class="icon-group">
+                        <div class="favorite-group">
+                            <button class="favorite" onclick="location.href='/login'">
+                                <img class="favorite_image" src="{{ asset('images/star.jpg') }}">
+                                <span>{{ $item->favorites_count }}</span>
+                            </button>
+                        </div>
+                        <form action="/comment" method="get">
+                            <input type="hidden" name="id" value="{{ $item['id'] }}">
+                            <input type="hidden" name="item_id" value="{{ $item['id'] }}">
+                            <button class="comment" type="submit" name="name" value="{{ $item['name'] }}">
+                                <img class="comment_image" src="{{ asset('images/comment.jpg') }}">
+                                <span>{{ $item->comments_count }}</span>
+                            </button>
+                        </form>
+                    </div>
+                    <div class="form__button" onclick="location.href='/login'">
+                        <input type="hidden" name="id" value="{{ $item['id'] }}">
+                        <button class="form__button-submit" type="submit">購入する</button>
+                    </div>
+                @endif
                 <div class="description-group">
                     <span class="label--item">商品説明</span><br><br>
                     <span class="form__label--item">{{ $item['description'] }}</span><br>
